9f1ff15817a6359da5188c5230971976e1acd8d6
dp.vanheerden@gmail.com <dp.vanheerden@gmail.com>
2018-05-29 19:47:50 +0200

    added Background for Level_01 of Block Breaker

---
+++ Block Breaker/Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour {
+
+	private static MusicPlayer _instance;
+
+	public static MusicPlayer instance 
+  {
+		get {
+			if (_instance == null) {
+				_instance = GameObject.FindObjectOfType<MusicPlayer>();
+			}
+
+			return _instance;
+		}
+	}
+
+	// Use this for initialization
+	void Awake () {
+		if (_instance != null && _instance != this) {
+			Destroy(this.gameObject);
+		} else {
+			_instance = this;
+			GameObject.DontDestroyOnLoad(_instance);
+		}
+	}
+	
+}
