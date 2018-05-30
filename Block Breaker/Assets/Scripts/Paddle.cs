1b88bf6b1c8aeb331efa103b1fea8419e242ff42
dp.vanheerden@gmail.com <dp.vanheerden@gmail.com>
2018-05-30 20:59:41 +0200

    Added scripts for Ball, Brick & Paddle. Added Prefab for different types of Bricks and Material for 2d Physics.

---
+++ Block Breaker/Assets/Scripts/Paddle.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour {
+
+	const float minX = 0.5f;
+	const float maxX = 15.5f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		Vector3 paddlePos = new Vector3(CalculateX(), this.transform.position.y, 0f);
+
+		this.transform.position = paddlePos;
+	}
+
+	float CalculateX() {
+		// Single Game Unit = 16. Therefore relative position in window.
+		var X = Input.mousePosition.x / Screen.width * 16;
+
+		return Mathf.Clamp(X, minX, maxX);
+	}
+}
