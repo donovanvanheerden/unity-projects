9c8592336d7f3a298952c20d6ddcb45a258dba9b
dp.vanheerden@gmail.com <dp.vanheerden@gmail.com>
2018-06-04 22:05:13 +0200

    Added new Project "Laser Defender"

---
+++ Laser Defender/Assets/Entities/EnemyFormation/EnemySpawner.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour {
+	
+	public float speed = 0.5f;
+	public float padding = 0.5f;
+	public GameObject enemyPrefab;
+	public float width = 10f;
+	public float height = 5f;
+	private bool movingRight = true;
+
+	private float X_MIN = 0;
+	private float X_MAX = 0;
+
+	// Use this for initialization
+	void Start () {
+
+		float distance = this.GetComponent<Transform>().position.z - Camera.main.GetComponent<Transform>().position.z;
+		var leftBoundary = Camera.main.ViewportToWorldPoint(new Vector3(0,0,distance));
+		var rightBoundary = Camera.main.ViewportToWorldPoint(new Vector3(1,0,distance));
+
+		X_MIN = leftBoundary.x;
+		X_MAX = rightBoundary.x;
+
+		foreach (Transform t in this.GetComponent<Transform>()) {
+			var enemy = Instantiate(enemyPrefab, new Vector3(t.position.x, t.position.y,5), Quaternion.identity);
+			enemy.GetComponent<Transform>().parent = t;
+		}
+
+	}
+	
+	private void OnDrawGizmos() {
+		Gizmos.DrawWireCube(this.GetComponent<Transform>().position, new Vector3(width, height, 0));
+	}
+
+	private void Update() {
+		
+		this.GetComponent<Transform>().Translate(((movingRight ? Vector3.right : Vector3.left) * speed / 2f) * Time.deltaTime);
+
+		float rightEdgeOfFormation = this.GetComponent<Transform>().position.x + (0.5f * width);
+		float leftEdgeOfFormation = this.GetComponent<Transform>().position.x - (0.5f * width);
+
+		if (leftEdgeOfFormation < X_MIN) {
+			movingRight = true;
+		} else if (rightEdgeOfFormation > X_MAX) {
+			movingRight = false;
+		}
+	}
+
+}
---
+++ Laser Defender/Assets/Entities/Player/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed = 5f;
+	public float padding = 0.5f;
+
+	private float X_MIN = 0;
+	private float X_MAX = 0;
+
+	private Vector3 POSITION { get { return this.GetComponent<Transform>().position; }}
+
+	private void Start() {
+
+		float distance = POSITION.z - Camera.main.GetComponent<Transform>().position.z;
+		var leftMost = Camera.main.ViewportToWorldPoint(new Vector3(0,0,distance));
+		var rightMost = Camera.main.ViewportToWorldPoint(new Vector3(1,0,distance));
+
+		X_MIN = leftMost.x + padding;
+		X_MAX = rightMost.x - padding;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+		if (Input.GetKey(KeyCode.LeftArrow)) {
+			this.GetComponent<Transform>().Translate((Vector3.left * speed) * Time.deltaTime );
+		}
+		
+		if (Input.GetKey(KeyCode.RightArrow)) {
+			this.GetComponent<Transform>().Translate((Vector3.right * speed) * Time.deltaTime);
+		}
+
+		float newX = Mathf.Clamp(this.GetComponent<Transform>().position.x, X_MIN, X_MAX);
+		this.GetComponent<Transform>().position = new Vector3 (newX, POSITION.y, POSITION.z);
+
+	}
+}
